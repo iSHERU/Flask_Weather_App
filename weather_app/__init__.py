b27d9efa5d402f2cc6a98dbdf74c3ba8f918d516
Sher Shah <shershah414@gmail.com>
2024-08-04 14:31:11 +0500

    Set Up Databse so it will import the already created database. Also seted up the structure needed. Now the webapp shows a header and a Map

--- weather_app/__init__.py
+++ weather_app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from os import path
 from .models import db
 
 DB_NAME = "weather.db"
@@ -9,14 +10,21 @@ def create_app():
   app.config['SECRET_KEY'] = 'secret_key'
   app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  db.init_app(app)
 
   with app.app_context():
-    db.init_app(app)
     db.create_all()
-  db.init_app(app)
+
 
   from .views import views
 
   app.register_blueprint(views, url_prefix='/')
 
   return app
+
+
+
+def create_database(app):
+  if not path.exists('website/' + DB_NAME):
+    db.create_all(app=app)
+    print('Created Database!')
