8c79c9e8913bf2c1259468a0bcefed5e4335b8b0
Sher Shah <shershah414@gmail.com>
2024-08-04 22:26:53 +0500

    removed database as it was of no need in this app

--- weather_app/__init__.py
+++ weather_app/__init__.py
@@ -1,29 +1,11 @@
 from flask import Flask
 from os import path
-from .models import db
-
-DB_NAME = "weather.db"
-
 
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = 'secret_key'
-  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-  db.init_app(app)
-
-  with app.app_context():
-    db.create_all()
-
 
   from .views import views
-
   app.register_blueprint(views, url_prefix='/')
 
   return app
-
-
-
-def create_database(app):
-  if not path.exists('website/' + DB_NAME):
-    db.create_all(app=app)
